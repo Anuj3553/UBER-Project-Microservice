93f5eb884dfa96e1047dc645f24c7f3ed2e1b691
Anuj3553 <anujverma3553@gmail.com>
2025-07-03 11:31:51 +0530

    Implement clustering for stress service to enhance performance and reliability

--- stress.service.js
+++ stress.service.js
@@ -1,18 +1,39 @@
 const express = require('express');
-const app = express();
 const morgan = require('morgan');
+const cluster = require('cluster');
+const os = require('os');
 
-app.use(morgan('dev')); // Use morgan for logging HTTP requests
+if (cluster.isMaster) {
+    const numCPUs = os.cpus().length;
 
-app.get('/stress-test', (req, res) => {
-    for (let i = 0; i < 100000000000; i++) {
-        // Simulating a heavy computation
+    console.log(`Master process is running with PID: ${process.pid}`);
+    console.log(`Forking ${numCPUs} workers...`);
+
+    // Fork workers
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
     }
 
-    res.send('Hello, World!');
-});
+    // Listen for dying workers and replace them
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died. Forking a new one...`);
+        cluster.fork();
+    });
+} else {
+    const app = express();
 
+    app.use(morgan('dev')); // Use morgan for logging HTTP requests
 
-app.listen(3002, () => {
-    console.log('Stress service is running on http://localhost:3002');
-});
+    app.get('/stress-test', (req, res) => {
+        for (let i = 0; i < 100000000000; i++) {
+            // Simulating a heavy computation
+        }
+
+        res.send('Hello, World!');
+    });
+
+    const PORT = 3002;
+    app.listen(PORT, () => {
+        console.log(`Worker ${process.pid} is running on http://localhost:${PORT}`);
+    });
+}
