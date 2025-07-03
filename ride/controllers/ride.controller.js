750bc43667c2c40d8d76efcd2b918da07df988a2
Anuj3553 <anujverma3553@gmail.com>
2025-07-03 19:23:02 +0530

    feat: implement ride acceptance and new ride notification features in captain and ride modules

--- ride/controllers/ride.controller.js
+++ ride/controllers/ride.controller.js
@@ -1,14 +1,45 @@
 const rideModel = require('../models/ride.model');
+const { publishToQueue } = require('../service/rabbit');
 
-module.exports.createRide = async (req, res, next) => {
+module.exports.createRide = async (req, res) => {
     const { pickup, destination } = req.body;
 
     const newRide = new rideModel({
-        user: req.user._id,
+        user: req.user.id,
         pickup,
         destination
     });
 
+    publishToQueue("new-ride", JSON.stringify(newRide));
+
     await newRide.save();
-        
+
+    res.status(201).json({
+        message: 'Ride created successfully',
+        ride: newRide
+    });
+}
+
+module.exports.acceptRide = async (req, res) => {
+    const { rideId } = req.query;
+
+    const ride = await rideModel.findById(rideId);
+    if (!ride) {
+        return res.status(404).json({ message: 'Ride not found' });
+    }
+
+    
+    if (ride.captain) {
+        return res.status(400).json({ message: 'Ride already accepted by another captain' });
+    }
+    
+    ride.status = 'accepted';
+    await ride.save();
+
+    publishToQueue("ride-accepted", JSON.stringify(ride));
+
+    res.status(200).json({
+        message: 'Ride accepted successfully',
+        ride
+    });
 }
--- ride/middleware/auth.middleware.js
+++ ride/middleware/auth.middleware.js
@@ -27,3 +27,30 @@ module.exports.userAuth = async (req, res, next) => {
         return res.status(401).json({ message: 'Unauthorized', error: error.message });
     }
 }
+
+module.exports.captainAuth = async (req, res, next) => {
+    try {
+        const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const response = await axios.get(`${process.env.BASE_URL}/captain/profile`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+
+        const captain = response.data;
+
+        if (!captain) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
+        req.captain = captain;
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: 'Unauthorized', error: error.message });
+    }
+}
